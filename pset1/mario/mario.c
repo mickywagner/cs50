69645658cabdaef34da5d6631573819b2e8f121e
Michaela Wagner <mickywagner@gmail.com>
2021-01-16 22:23:58 +0000

    Create do loop to get height input between 1 and 8

--- pset1/mario/mario.c
+++ pset1/mario/mario.c
@@ -3,5 +3,13 @@
 
 int main(void)
 {
+    // Ask user for height
+    int height;
+    do
+    {
+        height = get_int("Staircase height? ");
+    } while (height < 1 | height > 8);
     
+    printf("%i\n", height);
+   
 }
