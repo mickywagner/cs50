1df9521e608ea55995cbfccc976c75a43f7a054c
Michaela Wagner <mickywagner@gmail.com>
2021-01-18 04:20:34 +0000

    Round changeOwed to cents, divide by type of coins and add gother for coin value

--- pset1/cash/cash.c
+++ pset1/cash/cash.c
@@ -1,16 +1,29 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
-     // Get user input for change owed-- non negative, float, not too large
     float changeOwed;
     do 
     {
-      changeOwed = get_float("Change owed: "); 
+        changeOwed = get_float("Change owed: "); 
     }
     while (changeOwed < 0);
+    
+    int changeOwedInCents = round(changeOwed * 100);
+    
+    int quarter = 25;
+    int dime = 10;
+    int nickel = 5;
+    int penny = 1;
    
-    printf("%f\n", changeOwed);
-    // Return / print fewest coins for change plus \n
+    int quarters = floor(changeOwedInCents / quarter);
+    int dimes = floor((changeOwedInCents - (quarters * quarter)) / dime);
+    int nickels = floor((changeOwedInCents - (quarters * quarter + dimes * dime)) / nickel);
+    int pennies = floor((changeOwedInCents - (quarters * quarter + dimes * dime + nickels * nickel) / penny));
+    
+    int numberOfCoins = quarters + dimes + nickels + pennies;
+    
+    printf("%i\n", numberOfCoins);
 }
