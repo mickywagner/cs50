23eb11d3d2b4fd2143704baf0b55de3c3c5dd054
Michaela Wagner <mickywagner@gmail.com>
2021-01-20 23:59:45 +0000

    Tabulates the correct years, but no checks for inputs

--- labs/population.c
+++ labs/population.c
@@ -7,8 +7,19 @@ int main(void)
     int initialPopulation = get_int("Start size: ");
     // TODO: Prompt for end size
     int endPopulation = get_int("End size: ");
-  
+
     // TODO: Calculate number of years until we reach threshold
+    int years = 0;
+    int currentPopulation = initialPopulation;
+    int births = 3;
+    int deaths = 4;
+
+    while(currentPopulation < endPopulation)
+    {
+       currentPopulation = currentPopulation + (currentPopulation / births) - (currentPopulation / deaths);
+       years++;
+    }
 
     // TODO: Print number of years
+    printf("%i\n", years);
 }
