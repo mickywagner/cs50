a5dc547a8af2bb7e6309e4c5eeb01ed65ddc36af
Michaela Wagner <mickywagner@gmail.com>
2021-01-20 23:50:21 +0000

    Prompt for initial and end population size

--- labs/population.c
+++ labs/population.c
@@ -4,9 +4,10 @@
 int main(void)
 {
     // TODO: Prompt for start size
-
+    int initialPopulation = get_int("Start size: ");
     // TODO: Prompt for end size
-
+    int endPopulation = get_int("End size: ");
+  
     // TODO: Calculate number of years until we reach threshold
 
     // TODO: Print number of years
