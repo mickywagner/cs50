d7925009870299dbe77cbf1605ed3936be0a8118
Michaela Wagner <mickywagner@gmail.com>
2021-01-21 22:12:03 +0000

    Prompt user for credit card number

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -3,6 +3,17 @@
 
 int main(void)
 {
+    // PROMPT USER FOR CREDIT CARD
+    long creditCardNumber;
+    do 
+    {
+        creditCardNumber = get_long("Number: ");
+    }
+    while (creditCardNumber < 0);
+    
+    printf("%ld\n", creditCardNumber);
+    
+    // PRINT IF VALID AND WHAT TYPE OF CARD
     
     // multiply every other digit by 2 starting with the 2nd to last number
     // then add the products together for sumOfMultiplied
