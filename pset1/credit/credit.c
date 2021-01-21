c7c3aecba52407572d9349dac30956edb7bd3804
Michaela Wagner <mickywagner@gmail.com>
2021-01-21 22:04:49 +0000

    Add skeleton for credit.c

---
+++ pset1/credit/credit.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    
+    // multiply every other digit by 2 starting with the 2nd to last number
+    // then add the products together for sumOfMultiplied
+    
+    // Add sumOfMultiplied to sum of digits starting from end that weren't multiplied
+    
+    // Total of sums % 10 == 0 is valid
+}
