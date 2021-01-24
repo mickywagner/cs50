6d92a1ef082d0d43d07a9d3ec9214f8a1937445e
Michaela Wagner <mickywagner@gmail.com>
2021-01-24 07:26:07 +0000

    Prompt user for text

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -4,10 +4,16 @@
 int main(void)
 {
     // TODO: Prompt user for TEXT with get_string
+    string text = get_string("Text: ");
     
     // TODO: Count letters, words, and sentences in text
     
     // TODO: Use Coleman-Liau formala to estimate grade level to nearest int
     
+    // index = 0.0588 * L - 0.296 * S - 15.8;
+    // L = average number of letters per 100 words
+    // s = average number of sentences per 100 words
+    
     // TODO: Print out "Grade X" X is the grade
+    
 }
