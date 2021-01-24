0ffdff3907ac84adb09bbf2009a5f841ab23dbfe
Michaela Wagner <mickywagner@gmail.com>
2021-01-24 07:18:33 +0000

    Outline for readability

---
+++ pset2/readability/readability.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt user for TEXT with get_string
+    
+    // TODO: Count letters, words, and sentences in text
+    
+    // TODO: Use Coleman-Liau formala to estimate grade level to nearest int
+    
+    // TODO: Print out "Grade X" X is the grade
+}
