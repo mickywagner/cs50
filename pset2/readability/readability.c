b46369d22da24d7b1e1c53cf86af84a1ac35b9e9
Michaela Wagner <mickywagner@gmail.com>
2021-01-24 07:40:08 +0000

    Calculate words and letters in text

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -1,30 +1,37 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int main(void)
 {
-    // TODO: Prompt user for TEXT with get_string
     string text = get_string("Text: ");
     
     // TODO: Count letters, words, and sentences in text
-    
+    int words = 1;
+    int letters = 0;
+    for(int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isspace(text[i])) words++;
+        if (isalpha(text[i])) letters++;
+    }
+    printf("%i letter(s)\n", letters);
+    printf("%i word(s)\n", words);
     // TODO: Use Coleman-Liau formala to estimate grade level to nearest int
     
     // index = 0.0588 * L - 0.296 * S - 15.8;
     // L = average number of letters per 100 words
     // s = average number of sentences per 100 words
     
-    // TODO: Print out "Grade X" X is the grade
-    if (gradeLevel < 1)
-    {
-        printf("Before Grade 1\n");
-    } else if (gradeLevel > 16)
-    {
-        printf("Grade 16+\n");
-    }
-    else
-    {
-        printf("Grade %i\n", gradeLevel);
-    }
+    // if (gradeLevel < 1)
+    // {
+    //     printf("Before Grade 1\n");
+    // } else if (gradeLevel > 16)
+    // {
+    //     printf("Grade 16+\n");
+    // }
+    // else
+    // {
+    //     printf("Grade %i\n", gradeLevel);
+    // }
 }
