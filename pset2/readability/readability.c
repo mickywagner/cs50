88bfb534bc5a38fc757fe3bdf42dea0facea666e
Michaela Wagner <mickywagner@gmail.com>
2021-01-27 03:20:02 +0000

    Cast words to float, fix typo sentence end condition, fix style50 spacing issues, all tests pass

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <math.h>
 
 int main(void)
 {
@@ -11,38 +12,47 @@ int main(void)
         text = get_string("Text: ");
     } 
     while (strlen(text) < 1);
-   
     
-    // TODO: Count letters, words, and sentences in text
+    // Count letters, words, and sentences in text
     int words = 1;
     int letters = 0;
     int sentences = 0;
     
-    for(int i = 0, n = strlen(text); i < n; i++)
+    for (int i = 0, n = strlen(text); i < n; i++)
     {
-        if (isspace(text[i])) words++;
-        if (isalpha(text[i])) letters++;
-        if (text[i] == '.' | text[i] == '!' | text[i] == '.') sentences++;
+        if (isspace(text[i]))
+        {
+            words++;
+        }
+        if (isalpha(text[i])) 
+        {
+            letters++;
+        }
+        if (text[i] == '.' | text[i] == '!' | text[i] == '?') 
+        {
+            sentences++;
+        }
     }
     
-    printf("%i letter(s)\n", letters);
-    printf("%i word(s)\n", words);
-    printf("%i sentence(s)\n", sentences);
-    // TODO: Use Coleman-Liau formala to estimate grade level to nearest int
+    // Use Coleman-Liau formala to estimate grade level to nearest int
     
-    // index = 0.0588 * L - 0.296 * S - 15.8;
-    // L = average number of letters per 100 words
-    // s = average number of sentences per 100 words
+    // Letters and Sentences per 100 words
+    float L = letters / (float) words * 100;
+    float S = sentences / (float) words * 100;
     
-    // if (gradeLevel < 1)
-    // {
-    //     printf("Before Grade 1\n");
-    // } else if (gradeLevel > 16)
-    // {
-    //     printf("Grade 16+\n");
-    // }
-    // else
-    // {
-    //     printf("Grade %i\n", gradeLevel);
-    // }
+    float index = 0.0588 * L - 0.296 * S - 15.8;    
+    int gradeLevel = round(index);
+    
+    if (gradeLevel < 1)
+    {
+        printf("Before Grade 1\n");
+    } 
+    else if (gradeLevel > 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", gradeLevel);
+    }
 }
