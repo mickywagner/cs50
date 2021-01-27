1537d4191efbaf52c89988ef6b3ec93eae7a4bbc
Michaela Wagner <mickywagner@gmail.com>
2021-01-27 02:51:17 +0000

    Get letters, words, and sentences

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -16,13 +16,18 @@ int main(void)
     // TODO: Count letters, words, and sentences in text
     int words = 1;
     int letters = 0;
+    int sentences = 0;
+    
     for(int i = 0, n = strlen(text); i < n; i++)
     {
         if (isspace(text[i])) words++;
         if (isalpha(text[i])) letters++;
+        if (text[i] == '.' | text[i] == '!' | text[i] == '.') sentences++;
     }
+    
     printf("%i letter(s)\n", letters);
     printf("%i word(s)\n", words);
+    printf("%i sentence(s)\n", sentences);
     // TODO: Use Coleman-Liau formala to estimate grade level to nearest int
     
     // index = 0.0588 * L - 0.296 * S - 15.8;
