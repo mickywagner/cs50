16e0cd22ae53ade15f7e7f3954dfb0a7a38c3737
Michaela Wagner <mickywagner@gmail.com>
2021-01-27 09:06:36 +0000

    Check that only one argument is provided

---
+++ pset2/caesar/caesar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2) 
+    {
+        printf("Only one argument is allowed\n");
+        return 1; 
+    }
+   
+}
